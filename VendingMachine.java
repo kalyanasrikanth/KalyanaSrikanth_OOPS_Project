bcb550e57bd60b16b66b6f177590d090b2cce28d
kalyanasrikanth <62359854+kalyanasrikanth@users.noreply.github.com>
2022-10-17 22:51:00 +0530

    Create VendingMachine.java

---
+++ VendingMachine.java
@@ -0,0 +1,24 @@
+package LabAssignment3.vendingMachine;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class VendingMachine {
+	Queue<MarieGoldBiscuit> marieGoldBiscuit = new LinkedList<>();
+	Queue<Coke> coke =  new LinkedList<>();
+	Queue<KitKat> kitKat =  new LinkedList<>();
+	Queue<Chikki> chikki =  new LinkedList<>();
+	Queue<SaltedLays> saltedLays =  new LinkedList<>();
+	
+	//Max Slots for a product in a Vending Machine.
+	int maxSlots = 5;
+	
+	//To load Vending Machine with the products.
+	void vendingMachineLoading(Queue newList, Product product) {
+		for(int i=0; i<maxSlots; i++) {
+			newList.add(product);
+		}
+	}
+
+}
