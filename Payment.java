002050e15daaab0946f60b99a3bf8105104670ba
kalyanasrikanth <62359854+kalyanasrikanth@users.noreply.github.com>
2022-10-17 22:49:04 +0530

    Create Payment.java

---
+++ Payment.java
@@ -0,0 +1,19 @@
+package LabAssignment3.vendingMachine;
+
+public class Payment {
+	private double amount;
+	
+	double payment(Cart cart, boolean cash) {
+		if(cart.getCartList().isEmpty()) {
+			throw new CartEmptyException("Your Cart is empty. Please add something to checkout.");
+		}
+		for(Product product:cart.getCartList()) {
+			amount += product.getPrice();
+		}
+		if(cash) {
+			return amount;
+		}else {
+			return (amount*2);
+		}
+	}
+}
